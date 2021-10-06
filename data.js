a7f07e38503b9e2dc6056d28431cde5df5dffb76
Nacho Hotz <juanhotz611@gmail.com>
2021-10-06 15:18:52 -0300

    initial commit

---
+++ data.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500,
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    },
+    {
+      title: 'Título de producto',
+      img: '',
+      brand: 'Marca',
+      price: 1234500
+    }
+  ]
+
+module.exports = products;
---
+++ index.js
@@ -0,0 +1,98 @@
+var products = [
+  {
+    id: 1,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 2,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 3,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 4,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 5,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 6,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 7,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+  {
+    id: 8,
+    title: 'Título de producto',
+    img: '',
+    brand: 'Marca',
+    price: 1234500,
+  },
+];
+
+function createProduct(product) {
+  const productContainer = document.createElement('div');
+  productContainer.className = 'product';
+
+  const productImg = document.createElement('img');
+  productImg.id = 'img';
+  productImg.src = product.img;
+  productImg.alt = 'product';
+
+  const productTitle = document.createElement('h5');
+  productTitle.textContent = product.title;
+
+  const productBrand = document.createElement('p');
+  productBrand.textContent = product.brand;
+
+  const productPrice = document.createElement('p');
+  productPrice.id = 'price';
+  productPrice.textContent = `$ ${product.price}`;
+
+  productContainer.appendChild(productImg);
+  productContainer.appendChild(productTitle);
+  productContainer.appendChild(productBrand);
+  productContainer.appendChild(productPrice);
+
+  return productContainer;
+}
+
+function mapProducts(products) {
+  return products.map(createProduct);
+}
+
+function displayProducts() {
+  const productsContainer = document.querySelector('.product-container');
+
+  const arrayProducts = mapProducts(products);
+  arrayProducts.forEach((product) => productsContainer.appendChild(product));
+}
+
+displayProducts();
